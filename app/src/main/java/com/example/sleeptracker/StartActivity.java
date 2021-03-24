0ac6709ce03149d1857fabddfe0da728c52d8ba9
Igbooo <49489897+Igbooo@users.noreply.github.com>
2021-03-24 17:21:11 +0000

    Add home screen
    Implements the home screen (activity + fragment), which launches into the rest of the app

---
+++ app/src/main/java/com/example/sleeptracker/StartActivity.java
@@ -0,0 +1,21 @@
+package com.example.sleeptracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sleeptracker.ui.main.StartFragment;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, StartFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
