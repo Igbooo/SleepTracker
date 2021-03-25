8260d5af70bffa97a47970940ad474d05f496272
Igbooo <49489897+Igbooo@users.noreply.github.com>
2021-03-25 14:53:48 +0000

    Refactor class files

--- app/src/main/java/com/example/sleeptracker/ui/sleep/SleepFragment.java
+++ app/src/main/java/com/example/sleeptracker/ui/sleep/SleepFragment.java
@@ -1,4 +1,4 @@
-package com.example.sleeptracker.ui.dashboard;
+package com.example.sleeptracker.ui.sleep;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.sleeptracker.R;
 
-public class DashboardFragment extends Fragment {
+public class SleepFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private SleepViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_stats, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        homeViewModel =
+                ViewModelProviders.of(this).get(SleepViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_sleep, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/sleeptracker/ui/stats/StatsFragment.java
+++ app/src/main/java/com/example/sleeptracker/ui/stats/StatsFragment.java
@@ -1,4 +1,4 @@
-package com.example.sleeptracker.ui.home;
+package com.example.sleeptracker.ui.stats;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.sleeptracker.R;
 
-public class HomeFragment extends Fragment {
+public class StatsFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private StatsViewModel statsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_sleep, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        statsViewModel =
+                ViewModelProviders.of(this).get(StatsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_stats, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        statsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
