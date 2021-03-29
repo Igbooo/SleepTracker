d1a4ac0a1d5070e7a43af8e14472d16d9d33988a
Igbooo <49489897+Igbooo@users.noreply.github.com>
2021-03-29 01:46:44 +0100

    Implement input screen MVP
    Implement the input screen complete with time pickers and text that updates. Save sleep button does nothing

--- app/src/main/java/com/example/sleeptracker/ui/sleep/SleepFragment.java
+++ app/src/main/java/com/example/sleeptracker/ui/sleep/SleepFragment.java
@@ -1,20 +1,29 @@
 package com.example.sleeptracker.ui.sleep;
 
+import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.sleeptracker.MainActivity;
 import com.example.sleeptracker.R;
 
+import android.text.format.DateFormat;
+import java.util.Calendar;
+
 public class SleepFragment extends Fragment {
+    Button buttonBedtime, buttonWakeUp;
+    TextView tvBedtime, tvWakeUp;
+    int bedHour, bedMinute, wakeHour, wakeMinute;
 
     private SleepViewModel homeViewModel;
 
@@ -23,13 +32,56 @@ public class SleepFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(SleepViewModel.class);
         View root = inflater.inflate(R.layout.fragment_sleep, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        tvBedtime = root.findViewById(R.id.bedtimeTimeText);
+        tvWakeUp = root.findViewById(R.id.wakeUpTimeText);
+        buttonBedtime = root.findViewById(R.id.downArrowBedtime);
+        buttonWakeUp = root.findViewById(R.id.downArrowWakeUp);
+
+        buttonBedtime.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        getActivity(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                bedHour = hourOfDay;
+                                bedMinute = minute;
+                                Calendar calendar = Calendar.getInstance();
+                                calendar.set(0,0,0,bedHour,bedMinute);
+                                tvBedtime.setText(DateFormat.format("hh:mm aa", calendar));
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.updateTime(bedHour,bedMinute);
+
+                timePickerDialog.show();
+            }
+        });
+
+        buttonWakeUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        getActivity(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                wakeHour = hourOfDay;
+                                wakeMinute = minute;
+                                Calendar calendar = Calendar.getInstance();
+                                calendar.set(0,0,0,wakeHour,wakeMinute);
+                                tvWakeUp.setText(DateFormat.format("hh:mm aa", calendar));
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.updateTime(wakeHour,wakeMinute);
+
+                timePickerDialog.show();
             }
         });
         return root;
     }
+
 }
