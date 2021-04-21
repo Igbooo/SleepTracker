a7328d054698f17ad36100cd85976fd330af71d0
Igbooo <49489897+Igbooo@users.noreply.github.com>
2021-04-21 22:58:20 +0100

    All of the graph stuff
    It's got a graph, navigation, date and calendar displays and all the orange stats your eyes can handle! Date display on the x axis seems impossible

--- app/src/main/java/com/example/sleeptracker/MainActivity.java
+++ app/src/main/java/com/example/sleeptracker/MainActivity.java
@@ -45,51 +45,51 @@ public class MainActivity extends AppCompatActivity {
             SleepRecord sleep = new SleepRecord();
             sleep.setStartTime("00:00");
             sleep.setEndTime("03:00");
-            sleep.setStartDate("01-01-2021");
-            sleep.setEndDate("01-01-2021");
+            sleep.setStartDate("2021-01-01");
+            sleep.setEndDate("2021-01-01");
             sleepDatabase.sleepDAO().addRecord(sleep);
             //sleepDatabase.sleepDAO().deleteRecord(sleep);
 
             SleepRecord sleep1 = new SleepRecord();
             sleep1.setStartTime("00:00");
             sleep1.setEndTime("03:00");
-            sleep1.setStartDate("02-01-2021");
-            sleep1.setEndDate("02-01-2021");
+            sleep1.setStartDate("2021-01-02");
+            sleep1.setEndDate("2021-01-02");
             sleepDatabase.sleepDAO().addRecord(sleep1);
 
             SleepRecord sleep2 = new SleepRecord();
             sleep2.setStartTime("00:00");
             sleep2.setEndTime("01:00");
-            sleep2.setStartDate("03-01-2021");
-            sleep2.setEndDate("03-01-2021");
+            sleep2.setStartDate("2021-01-03");
+            sleep2.setEndDate("2021-01-03");
             sleepDatabase.sleepDAO().addRecord(sleep2);
 
             SleepRecord sleep3 = new SleepRecord();
             sleep3.setStartTime("00:00");
             sleep3.setEndTime("01:00");
-            sleep3.setStartDate("04-01-2021");
-            sleep3.setEndDate("04-01-2021");
+            sleep3.setStartDate("2021-01-04");
+            sleep3.setEndDate("2021-01-04");
             sleepDatabase.sleepDAO().addRecord(sleep3);
 
             SleepRecord sleep4 = new SleepRecord();
             sleep4.setStartTime("00:00");
             sleep4.setEndTime("03:00");
-            sleep4.setStartDate("05-01-2021");
-            sleep4.setEndDate("05-01-2021");
+            sleep4.setStartDate("2021-01-05");
+            sleep4.setEndDate("2021-01-05");
             sleepDatabase.sleepDAO().addRecord(sleep4);
 
             SleepRecord sleep5 = new SleepRecord();
             sleep5.setStartTime("00:00");
             sleep5.setEndTime("01:00");
-            sleep5.setStartDate("06-01-2021");
-            sleep5.setEndDate("06-01-2021");
+            sleep5.setStartDate("2021-01-06");
+            sleep5.setEndDate("2021-01-06");
             sleepDatabase.sleepDAO().addRecord(sleep5);
 
             SleepRecord sleep6 = new SleepRecord();
             sleep6.setStartTime("00:00");
             sleep6.setEndTime("01:00");
-            sleep6.setStartDate("07-01-2021");
-            sleep6.setEndDate("07-01-2021");
+            sleep6.setStartDate("2021-01-07");
+            sleep6.setEndDate("2021-01-07");
             sleepDatabase.sleepDAO().addRecord(sleep6);
 
             Toast.makeText(this, "DEBUG: Database inserted & deleted data successfully!", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/sleeptracker/ui/stats/StatsFragment.java
+++ app/src/main/java/com/example/sleeptracker/ui/stats/StatsFragment.java
@@ -1,11 +1,13 @@
 package com.example.sleeptracker.ui.stats;
 
+import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,53 +21,45 @@ import com.example.sleeptracker.R;
 import com.example.sleeptracker.SleepRecord;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import static com.example.sleeptracker.MainActivity.sleepDatabase;
+import static java.time.format.TextStyle.FULL;
 import static java.time.temporal.ChronoUnit.HOURS;
 import static java.time.temporal.ChronoUnit.MINUTES;
 
-public class StatsFragment extends Fragment {
+public class StatsFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
     private StatsViewModel statsViewModel;
     List<SleepRecord> sleeps = sleepDatabase.sleepDAO().getDataFromDB();
-    List<Long> timesSlept = new ArrayList<Long>();
+
+    BarGraphSeries<DataPoint> series;
+    public static LocalDate currentViewedDate;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        for (int i=0; i<7; i++) {
-            SleepRecord currentSleep = sleeps.get(i);
-            LocalTime startTime = LocalTime.parse(currentSleep.getStartTime());
-            LocalTime endTime = LocalTime.parse(currentSleep.getEndTime());
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
-            String startTimeFormat = startTime.format(formatter);
-            String endTimeFormat = endTime.format(formatter);
-            Long timeSlept = MINUTES.between(startTime, endTime) / 60;
-
-            timesSlept.add(timeSlept);
-        }
-
-        statsViewModel =
-                ViewModelProviders.of(this).get(StatsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_stats, container, false);
+
+        statsViewModel = ViewModelProviders.of(this).get(StatsViewModel.class);
+
         GraphView graph = (GraphView) root.findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
-                new DataPoint(1, timesSlept.get(0)),
-                new DataPoint(2, timesSlept.get(1)),
-                new DataPoint(3, timesSlept.get(2)),
-                new DataPoint(4, timesSlept.get(3)),
-                new DataPoint(5, timesSlept.get(4)),
-                new DataPoint(6, timesSlept.get(5)),
-                new DataPoint(7, timesSlept.get(6))
-        });
+
+        DataPoint[] dataPoints = loadNewDate(root, "2021-01-01");
+        series = new BarGraphSeries<>(dataPoints);
+
         graph.addSeries(series);
         series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
             @Override
@@ -74,6 +68,7 @@ public class StatsFragment extends Fragment {
             }
         });
         series.setSpacing(50);
+        graph.getGridLabelRenderer().setNumHorizontalLabels(7);
         graph.getGridLabelRenderer().setHorizontalAxisTitle(getString(R.string.horizontal_axis_title));
         graph.getGridLabelRenderer().setVerticalAxisTitle(getString(R.string.vertical_axis_title));
         graph.getGridLabelRenderer().setPadding(75);
@@ -84,6 +79,111 @@ public class StatsFragment extends Fragment {
 
         graph.getViewport().setYAxisBoundsManual(true);
         graph.getViewport().setMinY(0);
+
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        view.findViewById(R.id.statsDate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(currentViewedDate.getYear(), (currentViewedDate.getMonthValue() - 1),
+                        currentViewedDate.getDayOfMonth());
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), StatsFragment.this, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.setOnDateSetListener(StatsFragment.this);
+                datePickerDialog.show();
+            }
+        });
+
+        view.findViewById(R.id.statsCalendar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(currentViewedDate.getYear(), (currentViewedDate.getMonthValue() - 1),
+                        currentViewedDate.getDayOfMonth());
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), StatsFragment.this, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.setOnDateSetListener(StatsFragment.this);
+                datePickerDialog.show();
+            }
+        });
+
+        view.findViewById(R.id.previousStatsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                series.resetData(loadNewDate(getView(), currentViewedDate.minusDays(7).toString()));
+            }
+        });
+
+        view.findViewById(R.id.nextStatsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                series.resetData(loadNewDate(getView(), currentViewedDate.plusDays(7).toString()));
+            }
+        });
+    }
+
+    private DataPoint[] loadNewDate(View view, String date) {
+        final TextView dayOfWeekTV = (TextView) view.findViewById(R.id.statsDate);
+        List<Long> timesSlept = new ArrayList<Long>();
+
+        currentViewedDate = LocalDate.parse(date);
+
+        LocalDate lDate = LocalDate.parse(date);
+        String dayOfWeek = lDate.getDayOfWeek().getDisplayName(FULL, Locale.ENGLISH);
+        dayOfWeekTV.setText(lDate.getDayOfMonth() + "/" + lDate.getMonthValue() + "/" + lDate.getYear());
+
+        ArrayList<LocalDate> localDates = new ArrayList<LocalDate>();
+        for (int i=0; i<7; i++) {
+            localDates.add(lDate.plusDays(i));
+        }
+
+        for (LocalDate searchDate : localDates) { //for all dates this week
+            boolean found = false;
+
+            //for every entry in the DB
+            for (SleepRecord s : sleeps) {
+                LocalDate startDate = LocalDate.parse(s.getStartDate());
+                //if the sleep's date matches one we're trying to display
+                if (searchDate.equals(startDate)) {
+                    //work out the time slept and add it to a list of times
+                    LocalTime startTime = LocalTime.parse(s.getStartTime());
+                    LocalTime endTime = LocalTime.parse(s.getEndTime());
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
+                    String startTimeFormat = startTime.format(formatter);
+                    String endTimeFormat = endTime.format(formatter);
+                    Long timeSlept = MINUTES.between(startTime, endTime) / 60;
+
+                    timesSlept.add(timeSlept);
+                    found = true;
+                }
+            }
+
+            if (!found) { //if we never find a matching entry, set the time slept for that date to 0
+                timesSlept.add(0L);
+            }
+        }
+
+        DataPoint[] values = new DataPoint[7];
+
+        for (int i=0; i<7; i++) {
+            double x = i + 1;
+            double y = timesSlept.get(i);
+            DataPoint v = new DataPoint(x, y);
+            values[i] = v;
+        }
+
+        return values;
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        month++;
+        currentViewedDate = currentViewedDate.of(year, month, day);
+        series.resetData(loadNewDate(getView(), currentViewedDate.toString()));
     }
 }
